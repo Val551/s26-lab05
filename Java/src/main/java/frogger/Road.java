dd71f677aceb2c3c73310639281769998b73bf08
Fabio Campos <fabiocam@andrew.cmu.edu>
2026-02-12 21:36:54 -0500

    Refactor frogger and road class

--- Java/src/main/java/frogger/Road.java
+++ Java/src/main/java/frogger/Road.java
@@ -1,18 +1,31 @@
 package frogger;
 
+
 /**
- * Refactor Task 1.
- *
- * @author Zishen Wen (F22), Deyuan Chen (S22)
- */
+* Refactor Task 1.
+*
+* @author Zishen Wen (F22), Deyuan Chen (S22)
+*/
 public class Road {
-    private final boolean[] occupied;
+   private final boolean[] occupied;
 
-    public Road(boolean[] occupied) {
-        this.occupied = occupied;
-    }
 
-    public boolean[] getOccupied() {
-        return this.occupied;
-    }
+   public Road(boolean[] occupied) {
+       this.occupied = occupied;
+   }
+
+
+   public boolean isOccupied(int position) {
+       boolean[] occupied = this.occupied;
+       return occupied[position];
+   }
+  
+   public boolean isValid(int position) {
+       if (position < 0) return false;
+       boolean[] occupied = this.occupied;
+       return position < occupied.length;
+   }
 }
+
+
+
